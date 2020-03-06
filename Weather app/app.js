1aabb730b7f481d527148fa1a86f2d00a9f50a26
sne <snezhana.syngur@gmail.com>
2020-03-06 10:13:27 +0100

    Finish weather app

--- Weather app/app.js
+++ Weather app/app.js
@@ -1,9 +1,22 @@
-const weather = new Weather('Boston', 'MA');
 const ui = new UI();
-
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Miami', 'FL');
+// change location event
+document.getElementById('w-change-btn').addEventListener('click', (evt) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+    storage.setLocationData(city, state);
+
+    getWeather();
+    // Close modal
+    $('#locModal').modal('hide');
+});
+
 
 function getWeather() {
     weather.getWeather()
